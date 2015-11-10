d77b32523fdfe8db87954d7335be43fd2a18e778
herculex <sunpopoapollo@gmail.com>
2015-11-10 09:57:42 +0800

    alpha4

--- app/src/main/java/com/sumugu/liubo/lc/contract/ItemContract.java
+++ app/src/main/java/com/sumugu/liubo/lc/contract/ItemContract.java
@@ -15,14 +15,24 @@ public class ItemContract extends DbContract {
 
     //Columns specific constants
     public class Column{
-        public static final String ITEM_ID = BaseColumns._ID; // "_id"
+        //ID，取基础列的ID名称 "_id"
+        public static final String ITEM_ID = BaseColumns._ID;
+        //标题
         public static final String ITEM_TITLE="item_title";
+        //内容
         public static final String ITEM_CONTENT="item_content";
+        //创建时间
         public static final String ITEM_CREATED_AT="item_created_at";
+        //完成状态
         public static final String ITEM_IS_FINISHED="item_is_finished";
+        //闹钟状态
         public static final String ITEM_HAS_CLOCK="item_has_clock";
+        //设置闹钟
         public static final String ITEM_ALARM_CLOCK="item_alarm_clock";
+        //所属列表的ID
         public static final String ITEM_LIST_ID="item_list_id";
+        //排序的序号
+        public static final String ITEM_INDEX="item_index";
 
     }
 
