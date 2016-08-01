01906abb379d1228e4395c1a1940f51fcdcafce5
herculex <sunpopoapollo@gmail.com>
2016-08-01 16:25:43 +0800

    xdemo03,xdemo,MyDragViewGroup

--- app/src/main/java/com/sumugu/liubo/lc/xdemo/Xdemo.java
+++ app/src/main/java/com/sumugu/liubo/lc/xdemo/Xdemo.java
@@ -13,7 +13,7 @@ public class Xdemo extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_xdemo02);
+        setContentView(R.layout.activity_xdemo03);
 
         myScrollView=(MyScrollView)findViewById(R.id.customScroller);
     }
