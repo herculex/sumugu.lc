9cfbff35564cd58a0fc38fb55dbc6ab2b8b65c83
herculex <sunpopoapollo@gmail.com>
2016-08-17 17:16:25 +0800

    xdemo03,xdemo,  listview dispatch and intercept touchevent

--- app/src/main/java/com/sumugu/liubo/lc/xdemo/MyListView.java
+++ app/src/main/java/com/sumugu/liubo/lc/xdemo/MyListView.java
@@ -2,6 +2,8 @@ package com.sumugu.liubo.lc.xdemo;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.ListView;
 
 /**
@@ -20,4 +22,24 @@ public class MyListView extends ListView {
         super(context, attrs, defStyleAttr);
     }
 
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        Log.d("MyListView", "dispatchTouchEvent:" + ev.getActionMasked());
+//        if(ev.getActionMasked()==MotionEvent.ACTION_DOWN) {
+//            Log.d("MyListView","super.dispatch down");
+//            return super.dispatchTouchEvent(ev);
+//        }
+//
+//        if (ev.getActionMasked() == MotionEvent.ACTION_MOVE)
+            if(getFirstVisiblePosition() == 0 && getLastVisiblePosition() == (getChildCount() - 1)){
+                Log.d("MyListView","does not dispatch");
+                return false;}
+            else {
+                Log.d("MyListView","super.dispatch move");
+                return super.dispatchTouchEvent(ev);
+            }
+//
+//        Log.d("MyListView","super.dispatch");
+//        return super.dispatchTouchEvent(ev);
+    }
 }
