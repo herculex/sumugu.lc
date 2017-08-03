7e9a2a50b8fb566999e89d040e7c94f84a8def5e
herculex <sunpopoapollo@gmail.com>
2017-08-03 11:20:58 +0800

    contract,itemContract appended one column - ITEM_CREATED_AT_DAY

--- app/src/main/java/com/sumugu/liubo/lc/contract/ItemContract.java
+++ app/src/main/java/com/sumugu/liubo/lc/contract/ItemContract.java
@@ -12,6 +12,16 @@ public class ItemContract extends DbContract {
     public static final String TABLE="item";
     //default sort by item_created_at
     public static final String DEFAULT_SORT=Column.ITEM_CREATED_AT+" DESC";
+    // provider specific constants
+    // content://com.sumugu.liubo.lc.ItemProvider/item
+    public static final String AUTHORITY = "com.sumugu.liubo.lc.provider.ItemProvider";
+    public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + TABLE); //CONTENT_URI=content://com.sumugu.liubo.lc.ItemProvider/item
+    public static final int ITEM_ITEM = 1;
+    public static final int ITEM_DIR = 2;
+    //单个条目
+    public static final String ITEM_TYPE_ITEM = "vnd.android.cursor.item/vnd.com.sumugu.liubo.lc.provider.item";
+    //所有条目的目录
+    public static final String ITEM_TYPE_DIR = "vnd.android.cursor.dir/vnd.com.sumugu.liubo.lc.provider.item";
 
     //Columns specific constants
     public class Column{
@@ -19,22 +29,12 @@ public class ItemContract extends DbContract {
         public static final String ITEM_TITLE="item_title";
         public static final String ITEM_CONTENT="item_content";
         public static final String ITEM_CREATED_AT="item_created_at";
+        public static final String ITEM_CREATED_AT_DAY = "item_created_at_day";
         public static final String ITEM_IS_FINISHED="item_is_finished";
         public static final String ITEM_HAS_CLOCK="item_has_clock";
         public static final String ITEM_ALARM_CLOCK="item_alarm_clock";
         public static final String ITEM_LIST_ID="item_list_id";
 
     }
-
-    // provider specific constants
-    // content://com.sumugu.liubo.lc.ItemProvider/item
-    public static final String AUTHORITY="com.sumugu.liubo.lc.provider.ItemProvider";
-    public static final Uri CONTENT_URI=Uri.parse("content://"+AUTHORITY+"/"+TABLE); //CONTENT_URI=content://com.sumugu.liubo.lc.ItemProvider/item
-    public static final int ITEM_ITEM=1;
-    public static final int ITEM_DIR=2;
-    //单个条目
-    public static final String ITEM_TYPE_ITEM="vnd.android.cursor.item/vnd.com.sumugu.liubo.lc.provider.item";
-    //所有条目的目录
-    public static final String ITEM_TYPE_DIR="vnd.android.cursor.dir/vnd.com.sumugu.liubo.lc.provider.item";
 
 }
