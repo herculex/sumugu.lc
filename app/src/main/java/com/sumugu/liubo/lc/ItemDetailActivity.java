0b4ff82b8c38836ce99074b14398949d45186f93
herculex <sunpopoapollo@gmail.com>
2017-09-03 09:08:20 +0800

    continue to itemdetailactivity itemfragment for notification on click --- Do one thing Do it well..!

--- app/src/main/java/com/sumugu/liubo/lc/ItemDetailActivity.java
+++ app/src/main/java/com/sumugu/liubo/lc/ItemDetailActivity.java
@@ -1,55 +1,34 @@
 package com.sumugu.liubo.lc;
 
-import android.content.ClipData;
-import android.content.ContentUris;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.nfc.Tag;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateUtils;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.sumugu.liubo.lc.alarmclock.AlarmUntils;
-import com.sumugu.liubo.lc.contract.ItemContract;
-
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import com.sumugu.liubo.lc.simpleWay.ItemPackageActivity;
 
 public class ItemDetailActivity extends AppCompatActivity{
     private static final String TAG = ItemDetailActivity.class.getSimpleName();
-
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_detail);
 
-    }
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent main = new Intent(ItemDetailActivity.this, ItemPackageActivity.class);
+                startActivity(main);
 
+                finish();
+                overridePendingTransition(R.anim.in_from_left, R.anim.out_to_right);
+            }
+        });
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_item_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return super.onOptionsItemSelected(item);
     }
 
 }
